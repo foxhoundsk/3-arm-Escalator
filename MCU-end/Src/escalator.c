ed5029735527af0669804665415d0ea4457235e4
flawless0714 <flawless0714@gmail.com>
2018-11-27 16:55:56 +0800

    Update IR characteristic to latest version
    We finally get latest version from hospital by teacher.

--- MCU-end/Src/escalator.c
+++ MCU-end/Src/escalator.c
@@ -6,21 +6,21 @@ volatile Escalator escalator;
 extern volatile uint8_t CONVERSION_COMPLETE;
 //extern volatile uint16_t xdata adc_buf[3];
 extern volatile Wifi wifi;
-extern volatile Uart uart;
 extern volatile scan_t xdata adc_buf[1];
 const uint16_t SPEED_TABLE[6] = { 0x0, 0x0106, 0x020d, 0x03b1, 0x0521, 0x0628};
 
-/* TODO: if very early state the escalator should run? A: don't know how a DAC with 0 can run */
+/* TODO: if very early state the escalator should run? Technically, should not,
+         since it may change the pos val at certain circumstance, but we haven't
+         encountered so far */
 void escalatorProcess(void)
 {   
     uint8_t index, index2; /* for loop and array index use (WARN) */
-    uint8_t shiftedADC;
-	/*	WARN: debug use (known issue: this section should uncommented at release version(wifi ver), it prevent this func(escalatorProcess()) run before wifi module done its initialization and ready to send training data, although it is harmless) */
-    if (uart.Tstate == WAIT_KNOCK_DOOR)
+	/*	WARN: debug use
+    if (wifi.state != RUNNING_TRAINING)
     {
         return;
     }
-    
+    */
     if (escalator.intervalFlag == 1)
     {
         escalator.intervalFlag = 0;
@@ -38,12 +38,12 @@ void escalatorProcess(void)
         {
             case 0:                
                 if (0x3fd <= adc_buf[0].p1_1 && adc_buf[0].p1_1 <= 0x3ff)  escalator.arm[index].variability[POS_1of4]++;
-                else if (0x44 <= shiftedADC && shiftedADC <= 0x4a) escalator.arm[index].variability[POS_2of4]++;
-                else if (0x17 <= shiftedADC && shiftedADC <= 0x1d) escalator.arm[index].variability[POS_3of4]++;
-                else if (0xd8 <= shiftedADC && shiftedADC <= 0xde) escalator.arm[index].variability[POS_4of4]++;
+                else if (0x0170 <= adc_buf[0].p1_1 && adc_buf[0].p1_1 <= 0x01df) escalator.arm[index].variability[POS_2of4]++;
+                else if (0x00a0 <= adc_buf[0].p1_1 && adc_buf[0].p1_1 <= 0x0100) escalator.arm[index].variability[POS_3of4]++;
+                //else if (0x00a0 <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x0100) escalator.arm[index].variability[POS_4of4]++;
                 for (index2 = 0; index2 < 4; index2++)
                 {
-                    if (escalator.arm[index].variability[index2] >= 1000)
+                    if (escalator.arm[index].variability[index2] >= 3000)
                     {
                         escalator.arm[index].variability[POS_1of4] = 0; /* clear all variability to prevent near-side trigger*/
 												escalator.arm[index].variability[POS_2of4] = 0;
@@ -60,14 +60,13 @@ void escalatorProcess(void)
                 break;
 					
             case 1:
-                //shiftedADC = (uint8_t) (adc_buf[0].p1_2 >> 8);
                 if (0x3fd <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x3ff)  escalator.arm[index].variability[POS_1of4]++;
-                else if (0x010a <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x0110) escalator.arm[index].variability[POS_2of4]++;
-                else if (0x00c8 <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x00ce) escalator.arm[index].variability[POS_3of4]++;
-                else if (0x00af <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x00b5) escalator.arm[index].variability[POS_4of4]++;
+                /*0x256*/else if (0x0170 <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x01df) escalator.arm[index].variability[POS_2of4]++;
+                /*0x157*/else if (0x00a0 <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x0100) escalator.arm[index].variability[POS_3of4]++;
+                ///*0x0d9*/else if (0x00d8 <= adc_buf[0].p1_2 && adc_buf[0].p1_2 <= 0x00da) escalator.arm[index].variability[POS_4of4]++;
                 for (index2 = 0; index2 < 4; index2++)
                 {
-                    if (escalator.arm[index].variability[index2] >= 1000)
+                    if (escalator.arm[index].variability[index2] >= 3000)
                     {
                         escalator.arm[index].variability[POS_1of4] = 0;
 												escalator.arm[index].variability[POS_2of4] = 0;
@@ -84,12 +83,12 @@ void escalatorProcess(void)
                 break;
             case 2:   
                 if (0x3fd <= adc_buf[0].p1_3 && adc_buf[0].p1_3 <= 0x3ff)  escalator.arm[index].variability[POS_1of4]++;
-                else if (0x44 <= shiftedADC && shiftedADC <= 0x4a) escalator.arm[index].variability[POS_2of4]++;
-                else if (0x17 <= shiftedADC && shiftedADC <= 0x1d) escalator.arm[index].variability[POS_3of4]++;
-                else if (0xd8 <= shiftedADC && shiftedADC <= 0xde) escalator.arm[index].variability[POS_4of4]++;
+                else if (0x0170 <= adc_buf[0].p1_3 && adc_buf[0].p1_3 <= 0x01df) escalator.arm[index].variability[POS_2of4]++;
+                else if (0x00a0 <= adc_buf[0].p1_3 && adc_buf[0].p1_3 <= 0x0100) escalator.arm[index].variability[POS_3of4]++;
+                //else if (0x00d8 <= adc_buf[0].p1_3 && adc_buf[0].p1_3 <= 0x00da) escalator.arm[index].variability[POS_4of4]++;
                 for (index2 = 0; index2 < 4; index2++)
                 {
-                    if (escalator.arm[index].variability[index2] >= 1000)
+                    if (escalator.arm[index].variability[index2] >= 3000)
                     {
                         escalator.arm[index].variability[POS_1of4] = 0;
 												escalator.arm[index].variability[POS_2of4] = 0;
